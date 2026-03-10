cd21958ae57b3333e3c640165a37815c37eb0811
Rafaelht0 <heltonrafaelctt@gmail.com>
2026-03-10 20:54:22 -0300

    implementação do sistema de encomendas com cadastro, listagem, atualização de status e exclusão de pedidos

--- Sistemas-encomendas/app.py
+++ Sistemas-encomendas/app.py
@@ -2,6 +2,11 @@ import streamlit as st
 from pedidos import contar_pedidos, contar_pendentes, contar_entregues
 from pedidos import adicionar_pedido
 from database import criar_tabela
+from pedidos import listar_pedidos
+import pandas as pd
+from pedidos import listar_pedidos, marcar_entregue
+from pedidos import excluir_pedido
+
 
 criar_tabela()
 
@@ -9,7 +14,7 @@ st.title("📦 Sistemas de Encomendas")
 
 menu = st.sidebar.selectbox(
      "Menu",
-     ["Dashboard", "Novo pedido"] 
+     ["Dashboard", "Novo pedido", "Ver pedidos"] 
 
      )
 
@@ -37,3 +42,32 @@ if menu == "Novo pedido":
         adicionar_pedido(cliente, produto, quantidade)
         st.success("Pedido cadastrado!")
 
+if menu == "Ver pedidos":
+    st.subheader("Lista de pedidos")
+    pedidos = listar_pedidos()
+
+    for pedido in pedidos:
+        id_pedido = pedido[0]
+        cliente = pedido[1]
+        produto = pedido[2]
+        quantidade = pedido[3]
+        status = pedido[4]
+
+        col1, col2, col3, col4, col5, col6 = st.columns(6)
+
+        col1.write(id_pedido)
+        col2.write(cliente)
+        col3.write(produto)
+        col4.write(quantidade)
+        col5.write(status)
+
+        if status == "pendente":
+            if col6.button("Marcar como entregue", key=id_pedido):
+                marcar_entregue(id_pedido)
+                st.success("Pedido atualizado!")
+                st.rerun()
+
+        if col6.button("Excluir pedido", key=f"excluir_{id_pedido}"):
+            excluir_pedido(id_pedido)
+            st.success("Pedido excluído!")
+            st.rerun()
--- Sistemas-encomendas/pedidos.py
+++ Sistemas-encomendas/pedidos.py
@@ -34,3 +34,39 @@ def contar_entregues():
     total = cursor.fetchone()[0]
     conn.close()
     return total
+
+def listar_pedidos():
+    conn = conectar()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM pedidos")
+
+    pedidos = cursor.fetchall()
+    conn.close()
+    return pedidos
+
+def marcar_entregue(id_pedido):
+    conn =  conectar()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+                   UPDATE pedidos
+                   SET status = 'entregue'
+                   WHERE id = ?
+                   """, (id_pedido,))
+    
+    conn.commit()
+    conn.close()
+
+def excluir_pedido(id_pedido):
+
+    conn = conectar()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+                    DELETE FROM pedidos
+                    WHERE id = ?
+                    """, (id_pedido,))
+        
+    conn.commit()
+    conn.close()
